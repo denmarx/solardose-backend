f1e5366de9c733441fd21c2c56fb0b6fd4cd5048
undeadgorilla <dennis.marx93@gmail.com>
2024-11-27 14:06:15 -0800

    consolelogs7

--- utils/sunService.js
+++ utils/sunService.js
@@ -13,12 +13,12 @@ const hasNotificationBeenSentToday = (lastNotificationDate, userTimeZoneOffset) 
          const todayStartLocal = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
     console.log("todayStart Local Time:", todayStartLocal.toLocaleString());   
     // Adjust "todayStart" to UTC based on the user’s time zone offset
-    const todayStartUTC = new Date(todayStartLocal.getTime() - userTimeZoneOffset * 1000);  // Offset is in seconds, so multiply by 1000
-    console.log("todayStart (UTC):", todayStartUTC.toISOString());
+    // const todayStartUTC = new Date(todayStartLocal.getTime() - userTimeZoneOffset * 1000);  // Offset is in seconds, so multiply by 1000
+    // console.log("todayStart (UTC):", todayStartUTC.toISOString());
     const lastNotification = new Date(lastNotificationDate);
-    console.log("last notification:", lastNotification);
-    console.log("is last notification later than today start?", lastNotification >= todayStartUTC);
-    return lastNotification >= todayStartUTC;
+    // console.log("last notification:", lastNotification);
+    // console.log("is last notification later than today start?", lastNotification >= todayStartUTC);
+    return lastNotification >= todayStartLocal;
 } 
 
 
